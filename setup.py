2b0ba189ebfa022c80e981230e024c71836880b7
pietro <pietro.gra@hotmail.it>
2020-11-14 19:02:24 +0100

    setup improved and pipelines README

--- setup.py
+++ setup.py
@@ -9,9 +9,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="Standard-Traffic-Data",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Pietro Grandinetti",
+    description="Methods and Tools to standardize road traffic datasets.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pgrandinetti/standard-traffic-data",
